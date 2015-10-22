d6303c93d17c11a11ea0d466227a0191fad9784e
Luis Herranz <luisherranz@gmail.com>
2015-10-22 13:39:20 +0200

    won't fix #16 until we know if it is worth it

--- packages/appstate/tests/client/appstate-tests.js
+++ packages/appstate/tests/client/appstate-tests.js
@@ -830,19 +830,20 @@ Tinytest.add(
   }
 );
 
-Tinytest.add(
-  'MeteorFlux - AppState -  Should stop a computation not used anymore.',
-  function(test) {
-    beforeEach();
-
-    var number = new ReactiveVar(0);
-    AppState.set('a', function() {
-      number.set(1);
-      return number.get();
-    });
-    number.set(2);
-    AppState.set('a', 2);
-    Tracker.flush();
-    test.equal(number.get(), 2);
-  }
-);
+// Won't fix for now, until we see if this is really important.
+// Tinytest.add(
+//   'MeteorFlux - AppState -  Should stop a computation not used anymore.',
+//   function(test) {
+//     beforeEach();
+//
+//     var number = new ReactiveVar(0);
+//     AppState.set('a', function() {
+//       number.set(1);
+//       return number.get();
+//     });
+//     number.set(2);
+//     AppState.set('a', 2);
+//     Tracker.flush();
+//     test.equal(number.get(), 2);
+//   }
+// );
